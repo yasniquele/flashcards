be216a1f54c722963cbb4f486c1f0bb7b46e711b
yasniquele <yasmin.mugnaini@escola.pr.gov.br>
2025-05-28 23:07:58 +0000

    AULA 8

--- perguntas.js
+++ perguntas.js
@@ -15,3 +15,8 @@ criaCartao(
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
