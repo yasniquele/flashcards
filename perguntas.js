3bb998d98c105b3820e8271d706050614e80bb87
yasniquele <yasmin.mugnaini@escola.pr.gov.br>
2025-05-28 23:32:54 +0000

    AULA 9

--- perguntas.js
+++ perguntas.js
@@ -1,22 +1,59 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+  'Curiosidades sobre a Itália',
+  'O que significa o nome da Fontana di Trevi?',
+  'O nome vem de Tre Vie, que significa três ruas, e a tradição é jogar uma moeda de costas para a fonte com a mão direita por cima do ombro esquerdo'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+  'Curiosidades sobre a Itália',
+  'Como surgiu a massa na culinária italiana?',
+  'A massa surgiu quando civilizações antigas, como os etruscos, esmagavam cereais, misturavam com água e cozinhavam, criando um alimento nutritivo'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+  'Curiosidades sobre a Itália',
+  'Por que o vinho é importante na Itália?',
+  'O vinho está presente em todas as refeições e celebrações, a Itália é o maior exportador de vinho do mundo e tem mais de 350 tipos de uvas nativas'
 )
+
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+  'Curiosidades sobre a Itália',
+  'O que tem no Museu Galileu em Florença?',
+  'Lá estão expostos o dedo médio, o dedão e o dente de Galileu Galilei, encontrados por um colecionador de arte em 2010'
+)
+
+criaCartao(
+  'Curiosidades sobre a Itália',
+  'Quais são os vulcões ativos da Itália?',
+  'Stromboli e Etna, na Sicília, e Vesúvio, perto de Nápoles, sendo Vesúvio famoso por destruir Pompéia e Herculano em 79 d.C.'
+)
+
+criaCartao(
+  'Curiosidades sobre a Itália',
+  'É permitido cortar macarrão na Itália?',
+  'Cortar massas longas com faca é mal visto, o certo é enrolar com o garfo, usar colher é tolerado, mas cortar é considerado infantil'
+)
+
+criaCartao(
+  'Curiosidades sobre a Itália',
+  'Como é a população idosa da Itália?',
+  'A Itália tem a 3ª população mais idosa do mundo, com expectativa de vida mínima de 83 anos, reflexo da alimentação, saúde e cultura social'
+)
+
+criaCartao(
+  'Curiosidades sobre a Itália',
+  'O que significam as cores da bandeira italiana?',
+  'Verde representa natureza e esperança, branco simboliza fé e paz, e vermelho significa amor, coragem e os sacrifícios pela unificação'
+)
+
+criaCartao(
+  'Curiosidades sobre a Itália',
+  'Quem protege o Papa no Vaticano?',
+  '135 guardas suíços fazem a proteção do Papa desde 1506, reconhecidos pelo uniforme listrado e exigências como altura, fé cristã e treinamento militar'
+)
+
+criaCartao(
+  'Curiosidades sobre a Itália',
+  'Quanto tempo levou para construir a Torre de Pisa?',
+  'A torre levou 177 anos para ser concluída, começou a inclinar durante a construção por causa do solo instável, mas virou um dos pontos mais famosos do mundo'
 )
